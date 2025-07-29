867ab50161f882363a63c46b6fefff1fb2304091
Kyrilous Nasr <kyrilousnasr@gmail.com>
2025-07-29 18:23:46 -0400

    Configured admin.py, views.py, and models.py. Hooked up db queries to front-end, added superuser for testing.

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
     context = {}
     return render(request, 'store/store.html', context)
 
